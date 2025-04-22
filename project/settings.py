aa0a99a890bc05c38d536a19ea351ab77df89d46
David-git123 <dmpo@cesar.school>
2025-04-22 16:50:17 -0300

    Registrando os apps no settings

--- project/settings.py
+++ project/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app',
-    'rest_framework',
+    'home',
+    'painel_adm',
 ]
 
 MIDDLEWARE = [
