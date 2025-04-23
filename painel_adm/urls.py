60b70c86fb48fa0df911487e79c1c36d3c659dc4
David-git123 <dmpo@cesar.school>
2025-04-23 18:30:10 -0300

    Adicionando url de visualizar processo

--- painel_adm/urls.py
+++ painel_adm/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from painel_adm.views import InicioView,CriarCurso,EditarCurso,DeletarCurso,VisualizarCurso
+from painel_adm.views import InicioView,CriarCurso,EditarCurso,DeletarCurso,VisualizarCurso,VisualizarProcesso
 
 urlpatterns = [
     path('',InicioView.as_view(),name="inicio_painel"), 
@@ -8,5 +8,6 @@ urlpatterns = [
     path('visualizar_Curso/',VisualizarCurso.as_view(),name="visualiar_curso"),
     path('editar_curso/',EditarCurso.as_view(),name="editar_curso"),
     path('deletar_curso/',DeletarCurso.as_view(),name="deletar_curso"),
+    path('visualizar_processo/',VisualizarProcesso.as_view(),name="visualizar_processo"),
     path('django-admin/', admin.site.urls),  # Renomeado para evitar conflito com suas rotas admin personalizadas
 ]
