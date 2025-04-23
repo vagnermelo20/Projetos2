b4d92bc7945d1911d85045c2fe5ebb3ea83c0e43
David-git123 <dmpo@cesar.school>
2025-04-23 19:25:33 -0300

    Atualizando URlS

--- painel_adm/urls.py
+++ painel_adm/urls.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
-from painel_adm.views import InicioView,CriarCurso,EditarCurso,DeletarCurso,VisualizarCurso,VisualizarProcesso
+from painel_adm.views import InicioView,CriarCurso,EditarCurso,DeletarCurso,VisualizarCurso,VisualizarProcesso,EditarProcesso,DeletarProcesso
 
 urlpatterns = [
     path('',InicioView.as_view(),name="inicio_painel"), 
     path('criar_curso/',CriarCurso.as_view(),name="criar_curso"),
     path('visualizar_Curso/',VisualizarCurso.as_view(),name="visualiar_curso"),
-    path('editar_curso/',EditarCurso.as_view(),name="editar_curso"),
-    path('deletar_curso/',DeletarCurso.as_view(),name="deletar_curso"),
+    path('editar_curso/<int:curso_id>/',EditarCurso.as_view(),name="editar_curso"),
+    path('deletar_curso/<int:curso_id>/',DeletarCurso.as_view(),name="deletar_curso"),
+    path('editar_processo/<int:processo_id>/',EditarProcesso.as_view(),name="editar_processo"),
+    path('deletar_processo/<int:processo_id>/',DeletarProcesso.as_view(),name="deletar_processo"),
     path('visualizar_processo/',VisualizarProcesso.as_view(),name="visualizar_processo"),
     path('django-admin/', admin.site.urls),  # Renomeado para evitar conflito com suas rotas admin personalizadas
 ]
