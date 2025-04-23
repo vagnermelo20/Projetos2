fad06ad9892579c8b4af5661a3f622f7c0159614
David-git123 <dmpo@cesar.school>
2025-04-23 13:57:15 -0300

    Registrando os models dos 2 apps

--- home/admin.py
+++ home/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from home.models import Usuario
+from home.models import Usuario, AlunoRegistro
 
 admin.site.register(Usuario)
+admin.site.register(AlunoRegistro)
--- painel_adm/admin.py
+++ painel_adm/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from painel_adm.models import Curso
+from painel_adm.models import Curso,Selecao
 
 admin.site.register(Curso)
+admin.site.register(Selecao)
