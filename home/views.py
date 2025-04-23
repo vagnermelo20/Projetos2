ca01fa040e694ac2f0092f03a9bfa6a4ef71c48b
David-git123 <dmpo@cesar.school>
2025-04-23 14:30:53 -0300

    Criação das views de registro de aluno

--- home/views.py
+++ home/views.py
@@ -3,10 +3,9 @@ from django.shortcuts import render
 from django.shortcuts import render, redirect
 from django.views import View
 from django.contrib import messages
-from .models import Usuario
-from django.http import HttpResponse
+from home.models import Usuario,AlunoRegistro
 from django.contrib.auth.hashers import make_password, check_password
-from django.template import RequestContext
+
 
 
 class LoginView(View):
@@ -47,3 +46,98 @@ class LogoutView(View):
 class InicioView(View):
     def get(self, request):
         return render(request, 'home/home.html')
+
+class LoginView(View):
+    def get(self, request):
+        return render(request, 'home/login.html')
+
+    def post(self, request):
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+
+        if not email or not senha:
+            messages.error(request, 'Todos os campos são obrigatórios.')
+            return render(request, 'home/login.html')
+
+        try:
+            usuario = Usuario.objects.get(E_mail=email)
+            # Usar check_password para verificar a senha hasheada
+            if check_password(senha, usuario.Senha):
+                request.session['usuario_id'] = usuario.id
+                request.session.save()  # Forçar a gravação da sessão
+                # Redirecionar para a view de visualização de objetivos
+                return redirect('painel_adm')
+            else:
+                messages.error(request, 'Senha incorreta.')
+        except Usuario.DoesNotExist:
+            messages.error(request, 'Usuário não encontrado.')
+
+        return render(request, 'home/login.html')
+
+class LogoutView(View):
+    def get(self, request):
+        # Remover o ID do usuário da sessão
+        if 'usuario_id' in request.session:
+            del request.session['usuario_id']
+        
+        return redirect('inicio') 
+    
+class InicioView(View):
+    def get(self, request):
+        return render(request, 'home/home.html')
+
+class Registrar_aluno(View):
+    def get(self,request):
+        return render(request, 'home/registro_aluno.html')
+
+    def post(self,request):
+    
+        nome = request.POST.get('name')
+        telefone= request.POST.get('phone_number')
+        idade = request.POST.get('age')
+        bairro = request.POST.get('bairro')
+        educacao = request.POST.get('education')
+        periodo_estudo = request.POST.get('study_period')
+        curso_desejado = request.POST.get('desired_course')
+
+        if not nome or not telefone or not idade or not bairro or not educacao or not periodo_estudo or not curso_desejado:
+            messages.error(request, 'Todos os campos são obrigatórios.')
+            return render(request, 'home/registro_aluno.html')
+
+        if AlunoRegistro.objects.filter(Nome=nome).exists():
+            messages.error(request, 'Candidato já registado.')
+            return render(request, 'home/registro_aluno.html', {
+            'nome': nome,
+            'telefone': telefone,
+            'idade':idade,
+            'bairro':bairro,
+            'educacao':educacao,
+            'periodo_estudo':periodo_estudo,
+            'curso_desejado':curso_desejado
+        })
+        elif AlunoRegistro.objects.filter(Telefone=telefone).exists():
+            messages.error(request, 'Número já cadastrado. Insira outro número e tente novamente.')
+            return render(request, 'home/registro_aluno.html', {
+            'nome': nome,
+            'telefone': telefone,
+            'idade':idade,
+            'bairro':bairro,
+            'educacao':educacao,
+            'periodo_estudo':periodo_estudo,
+            'curso_desejado':curso_desejado
+        })
+        try:
+            AlunoRegistro.objects.create(
+                Nome=nome,
+                Telefone=telefone,
+                Idade=idade,
+                Bairro=bairro,
+                Educacao=educacao,
+                Periodo_estudo=periodo_estudo,
+                Curso_desejado=curso_desejado 
+            )
+            messages.success(request, 'O candidato foi registrado com sucesso!')
+            return render(request,'home/home.html')
+        except:
+            messages.success(request, 'Erro no registro do candidato!')
+            return render(request,'home/registro_aluno.html')
