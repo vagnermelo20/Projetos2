f04360bd2b21f7ed9aad20e258b1e9e0fab2fe9c
David-git123 <dmpo@cesar.school>
2025-04-23 17:06:16 -0300

    Adicionando url do template processo

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from home.views import InicioView,LoginView,AlunoRegistro
+from home.views import InicioView,LoginView,AlunoRegistro,ProcessoView
 
 urlpatterns = [
     path('',InicioView.as_view(), name='inicio'),
     path('login/',LoginView.as_view(), name='logar'),
     path('registro/',AlunoRegistro.as_views(),name='registro'),
+    path('processos/',ProcessoView.as_view(),name='processo')
 ]
