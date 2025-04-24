1839df8d64203f54c659e3b9f1a9152b8b5d25d2
David-git123 <dmpo@cesar.school>
2025-04-24 10:50:01 -0300

    Correção de URLS

--- painel_adm/urls.py
+++ painel_adm/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from painel_adm.views import InicioView,CriarCurso,EditarCurso,DeletarCurso,VisualizarCurso,VisualizarProcesso,EditarProcesso,DeletarProcesso
+from painel_adm.views import InicioView,CriarCurso,EditarCurso,DeletarCurso,VisualizarCurso,VisualizarProcesso,EditarProcesso,DeletarProcesso,CriarProcessoSeletivo
 
 urlpatterns = [
     path('',InicioView.as_view(),name="inicio_painel"), 
     path('criar_curso/',CriarCurso.as_view(),name="criar_curso"),
+    path('criar_processo',CriarProcessoSeletivo.as_view(),name="criar_processo"),
     path('visualizar_Curso/',VisualizarCurso.as_view(),name="visualizar_curso"),
     path('editar_curso/<int:curso_id>/',EditarCurso.as_view(),name="editar_curso"),
     path('deletar_curso/<int:curso_id>/',DeletarCurso.as_view(),name="deletar_curso"),
