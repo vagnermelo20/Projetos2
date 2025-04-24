5b673f846a5c4daa7afe0e77c29207451d8965ed
David-git123 <dmpo@cesar.school>
2025-04-24 08:50:55 -0300

    Adicionndo hasheamento de senha nos models

--- home/models.py
+++ home/models.py
@@ -1,13 +1,22 @@
 from django.db import models
 from painel_adm.models import Curso
+from django.contrib.auth.hashers import make_password
 
 class Usuario(models.Model):
     Username = models.CharField(max_length=100, unique=True)
     E_mail = models.EmailField(unique=True)
-    Senha = models.CharField(max_length=128)  
-
+    Senha = models.CharField(max_length=128)
+    
+    def save(self, *args, **kwargs):
+        # Hash the password before saving to the database
+        if self.Senha:
+            self.Senha = make_password(self.Senha)
+        super().save(*args, **kwargs)
+    
     def __str__(self):
         return self.Username
+    
+
 
 class AlunoRegistro(models.Model):
     Nome = models.CharField(max_length=255)
