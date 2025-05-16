a78197a360153c03e67878773fba6a82e73ca931
David-git123 <dmpo@cesar.school>
2025-05-16 19:25:02 -0300

    Registrando banco de dados Inscricoes

--- painel_adm/admin.py
+++ painel_adm/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from painel_adm.models import Curso,Selecao
+from painel_adm.models import Curso,Selecao,Inscricao
 
 admin.site.register(Curso)
 admin.site.register(Selecao)
+admin.site.register(Inscricao)
