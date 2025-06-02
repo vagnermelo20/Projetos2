fb21d716447cd68302c479a4c3fb5364a5825510
David-git123 <dmpo@cesar.school>
2025-06-02 16:58:32 -0300

    Adicionando campo de curso na parte do login

--- home/models.py
+++ home/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-from painel_adm.models import Curso
 from django.contrib.auth.hashers import make_password
 
 class Usuario(models.Model):
     Username = models.CharField(max_length=100, unique=True)
     tipos_conta_escolha=[("Admin","Admin"),("Professor","Professor")]
     Tipos_conta=models.CharField(max_length=30,choices=tipos_conta_escolha,null=True, blank=True)
+    Curso=models.CharField(max_length=100,null=True, blank=True)
     E_mail = models.EmailField(unique=True)
     Senha = models.CharField(max_length=128)
     
