2ee505b777e2dde42f5ad765414069c8077e66ba
David-git123 <dmpo@cesar.school>
2025-06-04 17:33:25 -0300

    Adicionando o modelo lote para o admin

--- painel_adm/admin.py
+++ painel_adm/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from painel_adm.models import Curso,Selecao,Inscricao
+from painel_adm.models import Curso,Selecao,Inscricao,Lote
 
 admin.site.register(Curso)
 admin.site.register(Selecao)
 admin.site.register(Inscricao)
+admin.site.register(Lote)
