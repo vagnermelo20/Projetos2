cee8b895e860c5c937c3d34e7b632b2e525c4848
Vagner Montenegro de Melo <vagnermontenegro20@gmail.com>
2025-06-04 09:33:10 -0300

    feito o teste de criar curso

--- painel_adm/tests.py
+++ painel_adm/tests.py
@@ -1,3 +1,86 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.test import LiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from home.models import Usuario
+from django.contrib.auth.hashers import make_password
 
-# Create your tests here.
+class TestCriarCurso(LiveServerTestCase):
+
+    def setUp(self):
+        # Cria um usuário no seu modelo customizado
+        self.usuario = Usuario.objects.create(
+            Username='testeuser',
+            E_mail='teste@email.com',
+            Senha='123',  # ou só: 'minhasenha123' e deixar o save() cuidar
+            Tipos_conta='Admin'
+        )
+
+        self.driver = webdriver.Chrome()
+
+
+    def tearDown(self):
+        self.driver.quit()
+
+    def test1_Criar_curso(self):
+        driver = self.driver  # ✅ Aqui está o que você queria
+
+        driver.get(self.live_server_url)
+
+        driver.find_element(By.LINK_TEXT, "Login").click()
+
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.ID, "campo_email"))
+        )
+
+        driver.find_element(By.ID, "campo_email").send_keys("teste@email.com")
+        time.sleep(3)  # Espera para garantir que o campo esteja preenchido
+        driver.find_element(By.ID, "campo_senha").send_keys("123")
+        time.sleep(3)  # Espera para garantir que o campo esteja preenchido
+
+        driver.find_element(By.TAG_NAME, "button").click()
+
+        time.sleep(5)
+        # 1: Criar curso sem nome
+        WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.LINK_TEXT, "Visualizar Cursos"))
+        ).click()
+        time.sleep(2)
+
+        WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.LINK_TEXT, "Criar curso"))
+        ).click()
+        time.sleep(2)
+
+        driver.find_element(By.ID, "campo_descricao").send_keys("Curso introdutório de programação em Python")
+        driver.find_element(By.ID, "campo_n_alunos").send_keys("30")
+        driver.find_element(By.TAG_NAME, "button").click()
+
+        WebDriverWait(driver, 10).until(
+            EC.text_to_be_present_in_element((By.TAG_NAME, "body"), "É necessário preencher todas as informações.")
+        )
+        time.sleep(2)
+        # 2: Criar curso correto
+        driver.find_element(By.ID, "campo_nome").send_keys("Curso de Python")
+        driver.find_element(By.ID, "campo_descricao").send_keys("Curso introdutório de programação em Python")
+        driver.find_element(By.ID, "campo_n_alunos").send_keys("30")
+        driver.find_element(By.TAG_NAME, "button").click()
+        time.sleep(2)
+        # 3: Criar curso com nome repetido
+        WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.LINK_TEXT, "Criar curso"))
+        ).click()
+        time.sleep(2)
+        driver.find_element(By.ID, "campo_nome").send_keys("Curso de Python")
+        driver.find_element(By.ID, "campo_descricao").send_keys("Curso introdutório de programação em Python")
+        driver.find_element(By.ID, "campo_n_alunos").send_keys("30")
+        driver.find_element(By.TAG_NAME, "button").click()
+        WebDriverWait(driver, 10).until(
+            EC.text_to_be_present_in_element((By.TAG_NAME, "body"), "'Você já tem um curso com este nome.")
+        )
+        time.sleep(2)
+
+
