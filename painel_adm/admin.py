0b17c07518bd8c32f38db94c8a32f33d99015c6b
David-git123 <dmpo@cesar.school>
2025-06-05 15:22:49 -0300

    .

--- painel_adm/admin.py
+++ painel_adm/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from painel_adm.models import Curso,Selecao,Inscricao,Lote,Turma
+from painel_adm.models import Curso,Selecao,Inscricao,Lote
 
 admin.site.register(Curso)
 admin.site.register(Selecao)
 admin.site.register(Inscricao)
 admin.site.register(Lote)
-admin.site.register(Turma)
+
